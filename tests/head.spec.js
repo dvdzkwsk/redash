6a74cddfa5912c006385510c234a197d3b3485bc
David Zukowski <zukoweb@gmail.com>
2015-11-20 14:39:23 -0500

    test(head): add basic tests

---
+++ tests/head.spec.js
@@ -0,0 +1,15 @@
+var head = Daedalus.head
+
+describe('(Function) head', function () {
+  it('Should be a function.', function () {
+    expect(head).to.be.a('function')
+  })
+  
+  it('Should return the first item in an array.', function () {
+    expect(head([1,2,3])).to.equal(1)
+  })
+  
+  it('Should return undefined if the array is empty.', function () {
+    expect(head([])).to.be.undefined
+  })
+})
