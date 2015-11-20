5b378a9f4dfc6437f19218461f99332a568d1d1a
David Zukowski <zukoweb@gmail.com>
2015-11-20 15:01:44 -0500

    test(reject): add basic tests

---
+++ tests/reject.spec.js
@@ -0,0 +1,18 @@
+var reject = Daedalus.reject
+
+describe('(Function) reject', function () {
+  it('Should be a function.', function () {
+    expect(reject).to.be.a('function')  
+  })
+  
+  it('Should be curried.', function () {
+    expect(reject()).to.be.a('function')
+  })
+  
+  it('Should include only items where the predicate does _not_ match.', function () {
+    var isLessThan3 = function (x) { return x < 3 }
+    
+    expect(reject(isLessThan3, [1,2,3,4,5])).to.deep.equal([3,4,5])
+    expect(reject(isLessThan3, [0,1,2])).to.deep.equal([])
+  })
+})
