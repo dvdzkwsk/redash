0441b328eb88863683fbe3de7447c035b03c3bd7
David Zukowski <zukoweb@gmail.com>
2015-11-20 12:19:58 -0500

    test(trace): add initial tests

---
+++ tests/trace.spec.js
@@ -0,0 +1,30 @@
+var trace = Daedalus.trace;
+
+describe('(Function) trace', function () {
+  var _spies;
+  
+  beforeEach(function () {
+    _spies = {};
+    _spies.noop = sinon.spy();
+  });
+  
+  it('Should be a function.', function () {
+    expect(trace).to.be.a('function');
+  });
+  
+  it('Should return a function.', function () {
+    expect(trace()).to.be.a('function');
+  });
+  
+  describe('The resulting function...', function () {
+    var _traced;
+    
+    beforeEach(function () {
+      _traced = trace('Sample trace');
+    });
+    
+    it('Should return the argument it receives.', function () {
+      expect(_traced('a')).to.equal('a');
+    });
+  });
+});
