48b81ac5d8f2289239fabf7c136f8278c6f4c4f2
David Zukowski <zukoweb@gmail.com>
2015-11-21 14:18:15 -0500

    test(takeUntil): add initial tests

---
+++ tests/takeUntil.spec.js
@@ -0,0 +1,32 @@
+var takeUntil = Daedalus.takeUntil
+
+describe('(Function) takeUntil', function () {
+  var _spies
+  
+  beforeEach(function () {
+    _spies = {}
+    _spies.not5 = sinon.stub()
+    _spies.not5.returns(false)
+    _spies.not5.withArgs(5).returns(true)  
+  })
+  
+  it('Should be a function.', function () {
+    expect(takeUntil).to.be.a('function')
+  })
+  
+  it('Should be curried.', function () {
+    expect(takeUntil()).to.be.a('function')
+  })
+  
+  it('Should return all items until the predicate matches (exclusive).', function () {
+    expect(takeUntil(_spies.not5)([1,2,3,4,5,6,7,8])).to.deep.equal([1,2,3,4])
+  })
+  
+  it('Should return a new array even if it\'s identical to the input.', function () {
+    var arr = [1,2,3,4]
+    var res = takeUntil(_spies.not5, [1,2,3,4])
+    
+    expect(res).to.deep.equal([1,2,3,4])
+    expect(res).to.not.equal(arr)
+  })
+})
