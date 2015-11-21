ca914f69bd7ad26e3eb0b00c8ff49c1c15113541
David Zukowski <zukoweb@gmail.com>
2015-11-21 13:56:33 -0500

    test(keys): add initial tests

---
+++ tests/keys.spec.js
@@ -0,0 +1,23 @@
+var keys = Daedalus.keys
+
+describe('(Function) keys', function () {
+  it('Should be a function.', function () {
+    expect(keys).to.be.a('function')
+  })
+  
+  it('Should return all own-props of an object.', function () {
+    function SomeClass () {}
+    SomeClass.prototype.foo = 'foo'
+    
+    var obj = new SomeClass()
+    obj.baz = '__baz'
+    obj.bar = '__bar'
+    
+    expect(keys(obj)).to.deep.equal(['baz', 'bar'])
+    expect(keys({
+      foo : '__foo',
+      bar : '__bar',
+      baz : '__baz'
+    })).to.deep.equal(['foo', 'bar', 'baz'])
+  })
+})
