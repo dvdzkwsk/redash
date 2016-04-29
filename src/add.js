d1fe7b56d5ddd85c8ad42aeca45e744597cec465
David Zukowski <zukoweb@gmail.com>
2016-04-29 17:45:22 -0400

    docs(add): add initial documentation

--- src/add.js
+++ src/add.js
@@ -1,5 +1,21 @@
 import _curry2 from './internal/_curry2'
 
+/**
+ * add : Number -> Number -> Number
+ *
+ * @description
+ * Returns the sum of two numbers.
+ *
+ * @when
+ * You should use this function when you wish to add two numbers together.
+ *
+ * @example
+ * _.add(5)(2) // 7
+ *
+ * @example
+ * const add3 = _.add(3)
+ * add3(5) // 8
+ */
 var add = _curry2(function add (a, b) {
   return a + b
 })
