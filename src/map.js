496ee957cde31f68e991e95a8f3deb90da365a10
David Zukowski <zukoweb@gmail.com>
2016-04-29 17:56:33 -0400

    docs(map): add initial documentation

--- src/map.js
+++ src/map.js
@@ -1,5 +1,28 @@
 import _curry2 from './internal/_curry2'
 
+/**
+ * add : (a -> b) -> [a] -> [b]
+ *
+ * @description
+ * Applies a transformation to every item in an array, returning a new
+ * array of the same size where each item has been transformed.
+ *
+ * @when
+ * You should use this function when you wish to apply a common transformation
+ * to every item in an array. For example, if you have an array of objects
+ * and want a new array containing only a certain property from each object.
+ *
+ * @example
+ * _.map((a) => a + 5, [1,2,3,4,5]) // [6,7,8,9,10]
+ *
+ * @example
+ * _.map((a) => a.id, [{ id: 1 }, { id: 2 }, { id: 3 }]) // [1,2,3]
+ *
+ * @example
+ * const add5 = _.add(5)
+ * const mapAdd5 = _.map(add5)
+ * mapAdd5([1,2,3,4]) // [6,7,8,9]
+ */
 var map = _curry2(function map (fn, xs) {
   var i   = 0
     , len = xs.length
