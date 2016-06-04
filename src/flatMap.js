91b3067a1a1729d34432609150d8f1fad0f7a1b4
David Zukowski <zukoweb@gmail.com>
2016-06-04 11:07:35 -0400

    refactor(flatMap): improve flattening performance

--- src/flatMap.js
+++ src/flatMap.js
@@ -3,13 +3,22 @@ import _curry2 from './internal/_curry2'
 var flatMap = _curry2(function flatMap (fn, xs) {
   var i   = 0
     , len = xs.length
-    , ys  = []
-  
-  for (; i < len; i++) { 
-    ys = ys.concat(fn(xs[i], i))
+    , bs  = []
+    , _i
+    , b
+
+  for (; i < len; i++) {
+    b = fn(xs[i])
+    if (Array.isArray(b)) {
+      for (_i = 0; _i < b.length; _i++) {
+        bs.push(b[_i])
+      }
+    } else {
+      bs.push(b)
+    }
   }
-  
-  return ys
+
+  return bs
 })
 
 export default flatMap
