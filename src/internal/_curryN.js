213645fd7ac9a9236a0d82bcb42604a273d6e3e8
David Zukowski <zukoweb@gmail.com>
2016-06-06 12:40:45 -0400

    chore(_curryN): cleanup definition and documentation

--- src/internal/_curryN.js
+++ src/internal/_curryN.js
@@ -2,13 +2,21 @@ import _slice from './_slice'
 import _arity from './_arity'
 
 /**
- * @description something
+ * @description
+ * Curries a function to the specified arity. Receives an array of arguments
+ * to treat as as already-supplied (meaning they count toward fulfilling
+ * the target arity). The resulting function will combine the existing
+ * arguments with those from the latest call, and if the resulting length
+ * is greater than or equal to the target arity, the original function will
+ * be called with those arguments. If the number of arguments is still
+ * smaller than the required amount, the function will be curried again.
+ *
  * @param {Number} arity - the target arity
  * @param {Number} applied - the array of already-applied arguments
  * @param {Function} fn - the function to be called once all arguments are supplied
  * @returns {Function}
  */
-function _curryN (arity, applied, fn) {
+var _curryN = function _curryN (arity, applied, fn) {
   return _arity(arity, function () {
     var newApplied = applied.concat(_slice.call(arguments))
 
