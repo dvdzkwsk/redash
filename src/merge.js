f57ce14ed8b46d48509f5e23b14e33ce8438ccba
David Zukowski <zukoweb@gmail.com>
2016-06-09 14:34:05 -0500

    refactor(merge): merge from L -> R

--- src/merge.js
+++ src/merge.js
@@ -1,17 +1,20 @@
 import _curry2 from './internal/_curry2'
 
+/**
+ * Merges all own properties of the first object into the second.
+ */
 var merge = _curry2(function merge (a, b) {
   var y = {}
     , prop
 
-  for (prop in a) {
-    if (a.hasOwnProperty(prop)) {
-      y[prop] = a[prop]
-    }
-  }
   for (prop in b) {
     if (b.hasOwnProperty(prop)) {
       y[prop] = b[prop]
+    }
+  }
+  for (prop in a) {
+    if (a.hasOwnProperty(prop)) {
+      y[prop] = a[prop]
     }
   }
   return y
