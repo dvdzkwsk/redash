7b4baf1b213f25eaed07854b9254fc7486522fc7
David Zukowski <zukoweb@gmail.com>
2016-06-08 22:55:11 -0500

    feat(without): add function

---
+++ src/without.js
@@ -0,0 +1,28 @@
+import _curry2 from './internal/_curry2'
+
+var without = _curry2(function without (as, bs) {
+  var ys    = []
+    , bi    = 0
+    , aslen = as.length
+    , bslen = bs.length
+    , ai
+    , b
+    , discard
+
+  for (; bi < bslen; bi++) {
+    b = bs[bi]
+    discard = false
+    for (ai = 0; ai < aslen; ai++) {
+      if (b == as[ai]) {
+        discard = true
+        break
+      }
+    }
+    if (!discard) {
+      ys.push(b)
+    }
+  }
+  return ys
+})
+
+export default without
