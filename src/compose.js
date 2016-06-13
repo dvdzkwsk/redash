18d3bc5f01fc34fdcc09922173894530825eebd8
David Zukowski <zukoweb@gmail.com>
2016-06-12 23:26:03 -0400

    refactor(compose): dispatch to pipe with reversed args

--- src/compose.js
+++ src/compose.js
@@ -1,4 +1,5 @@
-import curryN from './curryN'
+import _reverse from './internal/_reverse'
+import pipe from './pipe'
 
 /**
  * compose : (y -> z), ..., (g -> h), (a, b, ..., f -> g) -> (a, b, ..., f -> z)
@@ -6,18 +7,7 @@ import curryN from './curryN'
  * @since v0.1.0
  */
 var compose = function compose () {
-  var fns = arguments
-    , i   = fns.length - 1
-    , fn  = fns[i--]
-
-  return curryN(fn.length, function __composition__ () {
-    var y = fn.apply(null, arguments)
-
-    for (; i >= 0; i--) {
-      y = fns[i](y)
-    }
-    return y
-  })
+  return pipe.apply(this, _reverse.call(arguments))
 }
 
 export default compose
