31aa1b7eb6fd69590f6259a72b745d06c7b95dd2
David Zukowski <zukoweb@gmail.com>
2016-06-13 00:04:26 -0400

    fix(tail): do not attempt to call .slice directly on list

--- src/tail.js
+++ src/tail.js
@@ -1,10 +1,12 @@
+import _slice from './internal/_slice'
+
 /**
  * tail : [a] -> [a]
  *
  * @since v0.1.0
  */
 var tail = function tail (xs) {
-  return xs.slice(1)
+  return _slice.call(xs, 1)
 }
 
 export default tail
