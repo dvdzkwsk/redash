8f53e7d21a1a21f7b18e9ad4618a9b371b58a51c
David Zukowski <zukoweb@gmail.com>
2016-06-13 00:04:45 -0400

    fix(take): do not attempt to call .slice directly on list

--- src/take.js
+++ src/take.js
@@ -1,3 +1,4 @@
+import _slice from './internal/_slice'
 import _curry2 from './internal/_curry2'
 
 /**
@@ -6,7 +7,7 @@ import _curry2 from './internal/_curry2'
  * @since v0.1.0
  */
 var take = _curry2(function take (n, xs) {
-  return xs.slice(0, n)
+  return _slice.call(xs, 0, n)
 })
 
 export default take
