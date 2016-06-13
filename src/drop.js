7b01892f3483bd2de7a9214e0f86ab48cc76023d
David Zukowski <zukoweb@gmail.com>
2016-06-12 21:18:45 -0400

    docs(drop): add version tag

--- src/drop.js
+++ src/drop.js
@@ -1,6 +1,9 @@
 import _curry2 from './internal/_curry2'
 import _slice from './internal/_slice'
 
+/**
+ * @since v0.10.0
+ */
 var drop = _curry2(function drop (n, xs) {
   return _slice.call(xs, n)
 }, 'drop : Number -> Array -> Array')
