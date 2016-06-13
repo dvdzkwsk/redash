41a5a0d21f3356d4b58acc3c7f4c2a2e5c151afa
David Zukowski <zukoweb@gmail.com>
2016-06-13 10:51:40 -0400

    feat(has): add function

---
+++ src/has.js
@@ -0,0 +1,11 @@
+import _curry2 from './internal/_curry2'
+import _hasOwn from './internal/_hasOwn'
+
+/**
+ * has : String -> {k:v} -> Boolean
+ *
+ * @since v0.11.0
+ */
+export default _curry2(function as (k, o) {
+  return _hasOwn.call(o, k)
+})
