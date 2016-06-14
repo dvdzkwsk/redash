6139c99f1bba8ba78d29b7dfe3cac7b4726887b9
David Zukowski <zukoweb@gmail.com>
2016-06-14 08:52:04 -0400

    refactor(fromPairs): implement as reduce

--- src/fromPairs.js
+++ src/fromPairs.js
@@ -1,15 +1,11 @@
-import _arrayEach from './internal/_arrayEach'
+import reduce from './reduce'
 
 /**
  * fromPairs : [[k, v]] -> {k:v}
  *
  * @since v0.7.0
  */
-export default function fromPairs (xs) {
-  var y = {}
-
-  _arrayEach(function (x) {
-    y[x[0]] = x[1]
-  }, xs)
-  return y
-}
+export default reduce(function fromPairs (acc, x) {
+  acc[x[0]] = x[1]
+  return acc
+}, {})
