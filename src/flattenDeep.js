0c35e0f5a0ae762f386584da1b825daf6a667db6
David Zukowski <zukoweb@gmail.com>
2016-06-14 08:51:48 -0400

    refactor(flattenDeep): implement as reduce

--- src/flattenDeep.js
+++ src/flattenDeep.js
@@ -1,21 +1,17 @@
 import _concat from './internal/_concat'
-import _arrayEach from './internal/_arrayEach'
+import reduce from './reduce'
 
 /**
  * flattenDeep : [[a]] -> [a]
  *
  * @since v0.5.0
  */
-export default function flattenDeep (xs) {
-  var ys = []
+var flattenDeep = reduce(function flattenDeep (acc, x) {
+  if (Array.isArray(x)) {
+    return _concat.call(acc, flattenDeep(x))
+  }
+  acc.push(x)
+  return acc
+}, [])
 
-  _arrayEach(function (x) {
-    if (Array.isArray(x)) {
-      ys = _concat.call(ys, flattenDeep(x))
-    } else {
-      ys.push(x)
-    }
-  }, xs)
-
-  return ys
-}
+export default flattenDeep
