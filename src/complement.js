cb30c5fd3b75b3b04b3fd9ebb25bf784c522b5d8
David Zukowski <zukoweb@gmail.com>
2016-06-19 13:25:52 -0400

    fix(complement): return curried function

--- src/complement.js
+++ src/complement.js
@@ -1,10 +1,12 @@
+import curryN from './curryN'
+
 /**
  * complement : (*... -> Boolean) -> (*... -> Boolean)
  *
  * @since v0.9.0
  */
 export default function complement (fn) {
-  return function () {
+  return curryN(fn.length, function () {
     return !fn.apply(this, arguments)
-  }
+  })
 }
