856ce0f39aee8711e38d708402114e8d648a8337
David Zukowski <zukoweb@gmail.com>
2016-06-26 21:19:37 -0400

    fix(flatten): fix acc mutator breaking subsequent calls

--- src/flatten.js
+++ src/flatten.js
@@ -1,18 +1,25 @@
-import _arrayEach from './internal/_arrayEach'
-import reduce from './reduce'
-
 /**
  * flatten : [[a]] -> [a]
  *
  * @since v0.1.0
  */
-export default reduce(function flatten (acc, x) {
-  if (Array.isArray(x)) {
-    _arrayEach(function (x_) {
-      acc.push(x_)
-    }, x)
-  } else {
-    acc.push(x)
+export default function flatten (xs) {
+  var acc = []
+    , i   = 0
+    , len = xs.length
+    , x
+    , xi
+    , xlen
+
+  for (; i < len; i++) {
+    x = xs[i]
+    if (Array.isArray(x)) {
+      for (xi = 0, xlen = x.length; xi < xlen; xi++) {
+        acc.push(x[xi])
+      }
+    } else {
+      acc.push(x)
+    }
   }
   return acc
-}, [])
+}
