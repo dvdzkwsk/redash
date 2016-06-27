302f7d0c4212e10a4c55bf8280fe3edb4b36b573
David Zukowski <zukoweb@gmail.com>
2016-06-27 17:11:51 -0400

    refactor(sum): implement as standalone, not as reduce implementation

--- src/sum.js
+++ src/sum.js
@@ -1,9 +1,15 @@
-import add from './add'
-import reduce from './reduce'
-
 /**
  * sum : [Number] -> Number
  *
  * @since v0.10.0
  */
-export default reduce(add, 0)
+export default function sum (xs) {
+  var i   = 0
+    , len = xs.length
+    , sum = 0
+
+  for (; i < len; i++) {
+    sum += xs[i]
+  }
+  return sum
+}
