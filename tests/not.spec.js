d3e342dcb5044642414b887e65c8c8241c9a9dc6
David Zukowski <zukoweb@gmail.com>
2016-06-28 09:43:28 -0400

    test(not): add tests for function case

--- tests/not.spec.js
+++ tests/not.spec.js
@@ -1,6 +1,10 @@
 var not = Redash.not
 
 describe('(Function) not', function () {
+  it('Should properly report its arity (is unary)', function () {
+    not.should.have.length(1)
+  })
+
   it('Should invert the provided value (true -> false, false -> true)', function () {
     not(true).should.equal(false)
     not(false).should.equal(true)
@@ -20,5 +24,47 @@ describe('(Function) not', function () {
 
   it('Should handle `void`', function () {
     not(void 0).should.equal(true)
+  })
+
+  describe('If the received argument is a function...', function () {
+    it('Should return a function', function () {
+      not(function () {}).should.be.a('function')
+    })
+
+    describe('The returned function...', function () {
+      it('Should have the same arity as the original', function () {
+        not(function () {}).should.have.length(0)
+        not(function (a0) {}).should.have.length(1)
+        not(function (a0, a1) {}).should.have.length(2)
+        not(function (a0, a1, a2) {}).should.have.length(3)
+        not(function (a0, a1, a2, a3) {}).should.have.length(4)
+        not(function (a0, a1, a2, a3, a4) {}).should.have.length(5)
+      })
+
+      it('Should be curried', function () {
+        var fn = not(function (a0, a1, a2) {})
+
+        fn(1).should.be.a('function')
+        fn(1, 2).should.be.a('function')
+        fn(1, 2, 3).should.not.be.a('function')
+      })
+
+      it('Should return the boolean complement of the original return value', function () {
+        not(function () { return false })().should.equal(true)
+        not(function () { return true })().should.equal(false)
+      })
+
+      it('Should properly cast truthy/falsy values', function () {
+        not(function () { return '' })().should.equal(true)
+        not(function () { return null })().should.equal(true)
+        not(function () { return 0 })().should.equal(true)
+        not(function () { return undefined })().should.equal(true)
+
+        not(function () { return true })().should.equal(false)
+        not(function () { return {} })().should.equal(false)
+        not(function () { return [] })().should.equal(false)
+        not(function () { return 'hello' })().should.equal(false)
+      })
+    })
   })
 })
