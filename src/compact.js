25ad45cce3744fb9b51d3043cec83e4909ad2dad
David Zukowski <zukoweb@gmail.com>
2016-06-28 17:15:07 -0400

    feat(compact): add function

---
+++ src/compact.js
@@ -0,0 +1,15 @@
+/**
+ * compact : [a] -> [a]
+ */
+export default function compact (xs) {
+  var i   = 0
+    , len = xs.length
+    , res = []
+
+  for (; i < len; i++) {
+    if (xs[i]) {
+      res[res.length] = xs[i]
+    }
+  }
+  return res
+}
