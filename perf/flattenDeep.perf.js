cf43ebc85d6f5f7fd9ed96248cdcd6d35d8837bd
David Zukowski <zukoweb@gmail.com>
2016-06-29 22:43:48 -0400

    fix(perf): how about we actually run Ramda

--- perf/flattenDeep.perf.js
+++ perf/flattenDeep.perf.js
@@ -4,12 +4,12 @@ var Ramda     = require('ramda')
   , LodashFP  = require('lodash/fp')
   , benchmark = require('./utils/benchmark')
 
-var SAMPLE = Redash.times(() => ([1, 2, [3, [4], [5, 6]]]), 100)
+var SAMPLE = Redash.times(() => ([1, 2, [3, [4], [5, 6]]]), 10000)
 
 module.exports = function () {
   return {
     redash   : benchmark(() => Redash.flattenDeep(SAMPLE))
-  , ramda    : benchmark(() => Redash.flatten(SAMPLE))
+  , ramda    : benchmark(() => Ramda.flatten(SAMPLE))
   , lodash   : benchmark(() => Lodash.flattenDeep(SAMPLE))
   , lodashFP : benchmark(() => LodashFP.flattenDeep(SAMPLE))
   }
