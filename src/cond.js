86ac930920d12d27c52e81c5e4583a4262282cf1
David Zukowski <zukoweb@gmail.com>
2016-06-30 23:34:41 -0400

    docs(cond): fix signature

--- src/cond.js
+++ src/cond.js
@@ -1,17 +1,17 @@
 import _curry2 from './internal/_curry2'
 
 /**
- * cond : [[(a -> Boolean), (a -> *)]] -> a -> (a -> *)
+ * cond : [[(a -> Boolean), (a -> *)]] -> a -> *
  */
-export default _curry2(function cond (conditions, x) {
+export default _curry2(function cond (conditions, a) {
   var i   = 0
     , len = conditions.length
     , cond
 
   for (; i < len; i++) {
     cond = conditions[i]
-    if (cond[0](x)) {
-      return cond[1](x)
+    if (cond[0](a)) {
+      return cond[1](a)
     }
   }
 })
