376e38c1f5100960e994907bd5171fc08b4c196b
David Zukowski <zukoweb@gmail.com>
2016-07-03 15:23:12 -0400

    release(debug)

--- dist/redash.js
+++ dist/redash.js
@@ -174,18 +174,15 @@
 
   var _slice = [].slice
 
-  // Credit to Ramda for null idea for creating curried functions that
+  // Credit to Ramda for this idea for creating curried functions that
   // properly report their arity via function.length.
   // https://github.com/ramda/ramda/blob/master/src/internal/_arity.js
   function _arity (arity, fn) {
     switch (arity) {
-      case 0: return fn
-      case 1: return function (a0) { return fn(a0) }
-      case 2: return function (a0, a1) { return fn(a0, a1) }
-      case 3: return function (a0, a1, a2) { return fn(a0, a1, a2) }
-      case 4: return function (a0, a1, a2, a3) { return fn(a0, a1, a2, a3) }
-      case 5: return function (a0, a1, a2, a3, a4) { return fn(a0, a1, a2, a3, a4) }
-      case 6: return function (a0, a1, a2, a3, a4, a5) { return fn(a0, a1, a2, a3, a4, a5) }
+      // Arities 0 -> 3 are automatically handled by curry, _curry1, _curry2, and _curry3
+      case 4: return function (a0, a1, a2, a3) { return fn.apply(this, arguments) }
+      case 5: return function (a0, a1, a2, a3, a4) { return fn.apply(this, arguments) }
+      case 6: return function (a0, a1, a2, a3, a4, a5) { return fn.apply(this, arguments) }
       default: throw new Error('Arity must be less than or equal to 6.')
     }
   }
