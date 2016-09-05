29f8061558085c7dd76eb1f4972b8277e7862823
David Zukowski <zukoweb@gmail.com>
2016-09-05 13:35:20 -0500

    feat(subtract): add function

---
+++ src/subtract.js
@@ -0,0 +1,10 @@
+import _curry2 from './internal/_curry2'
+
+/**
+ * subtract : Number -> Number -> Number
+ *
+ * @since v0.13.0
+ */
+export default _curry2(function subtract (a, b) {
+  return b - a
+})
