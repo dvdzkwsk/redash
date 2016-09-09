7a904e09fb8bcf3b96c957942a1d45ccf020527c
David Zukowski <zukoweb@gmail.com>
2016-09-09 14:17:48 -0500

    feat(identical): add function

---
+++ src/identical.js
@@ -0,0 +1,5 @@
+import _curry2 from './internal/_curry2'
+
+export default _curry2(function identical (a, b) {
+  return a === b
+})
---
+++ tests/identical.spec.js
@@ -0,0 +1,30 @@
+const test          = require('ava')
+    , { identical } = require('../dist/stdlib') 
+
+test('properly reports its arity (is binary)', (t) => {
+  t.is(identical.length, 2)
+})
+
+test('is curried', (t) => {
+  t.is(typeof identical(1), 'function')
+})
+
+test('returns true if primitive values are the same', (t) => {
+  t.true(identical(1, 1))
+  t.true(identical('hello', 'hello'))
+})
+
+test('returns true if primitive values are different', (t) => {
+  t.false(identical(1, 0))
+  t.false(identical('hello', 'goodbye'))
+})
+
+test('returns true if the values occupy the same space in memory', (t) => {
+  const obj = {}
+
+  t.true(identical(obj, obj))
+})
+
+test('returns false if objects have the same exact shape and values, but occupy different spaces in memory', (t) => {
+  t.false(identical({}, {}))
+})
