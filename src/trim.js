01dc037728d3bf8a0985a17f1c0027d70e9304b8
David Zukowski <zukoweb@gmail.com>
2016-09-21 21:26:39 -0500

    feat(trim): add function

---
+++ src/trim.js
@@ -0,0 +1,11 @@
+/**
+ * trim : String -> String
+ *
+ * @since v0.13.0
+ *
+ * @param {String} a - the string to trim
+ * @returns {String} the trimmed string
+ */
+export default function trim (a) {
+  return a.trim()
+}
---
+++ tests/trim.spec.js
@@ -0,0 +1,18 @@
+const test     = require('ava')
+    , { trim } = require('../dist/fp-standard')
+
+test('properly reports its arity (is unary)', (t) => {
+  t.is(trim.length, 1)
+})
+
+test('trims leading whitespace', (t) => {
+  t.is(trim('    hello'), 'hello')
+})
+
+test('trims trailing whitespace', (t) => {
+  t.is(trim('hello     '), 'hello')
+})
+
+test('noops if there is no leading or trailing whitespace', (t) => {
+  t.is(trim('hello'), 'hello')
+})
