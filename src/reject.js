f88f455c425c296b6eb65943007d310bf6acef0d
David Zukowski <zukoweb@gmail.com>
2016-10-05 21:38:05 -0500

    refactor(reject): implement independently from filter/complement

--- src/reject.js
+++ src/reject.js
@@ -1,12 +1,21 @@
 import _curry2 from './internal/_curry2'
-import filter from './filter'
-import complement from './complement'
 
 /**
  * reject : (a -> Boolean) -> [a] -> [a]
  *
  * @since v0.1.0
  */
-export default _curry2(function reject (fn, xs) {
-  return filter(complement(fn), xs)
+export default _curry2(function reject (pred, as) {
+  var i   = 0
+    , len = as.length
+    , res = []
+    , a
+
+  while (i < len) {
+    a = as[i++]
+    if (!pred(a)) {
+      res[res.length] = a
+    }
+  }
+  return res
 })
