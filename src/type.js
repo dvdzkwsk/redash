01e7a255ef67708d44244e1c88e75138c23293db
David Zukowski <zukoweb@gmail.com>
2016-10-15 14:14:31 -0500

    chore(type): use internal _toString

--- src/type.js
+++ src/type.js
@@ -1,7 +1,9 @@
-var _toString = Object.prototype.toString
+import _toString from './internal/_toString'
 
 /**
+ * @name type
  * @signature a -> String
+ * @since v0.13.0
  */
 export default function type (a) {
   var _type = a == null ? 'Nil' : _toString.call(a).slice(8, -1)
