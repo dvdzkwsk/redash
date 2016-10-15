ebc1d54521096b69880f942ff133e4831cb60ecf
David Zukowski <zukoweb@gmail.com>
2016-10-15 18:44:07 -0500

    feat(dropUntil): add function

---
+++ src/dropUntil.js
@@ -0,0 +1,25 @@
+import _curry2 from './internal/_curry2'
+import _slice from './internal/_slice'
+
+/**
+ * @name dropUntil
+ * @signature (a -> Boolean) -> [a] -> [a]
+ * @since v0.14.0
+ * @description
+ * Runs a predicate function sequentially through an array, dropping
+ * all elements until the predicate returns true. Once the predicate
+ * returns true, the remainder of the array is returned.
+ *
+ * @example
+ * const isEven = x => x % 2 === 0
+ *
+ * dropUntil(isEven, [1, 3, 6, 8, 10]) // => [6, 8, 10]
+ * dropUntil(isEven, [1, 3, 6, 5, 7])  // => [6, 5, 7]
+ */
+export default _curry2(function dropUntil (fn, xs) {
+  var i   = 0
+    , len = xs.length
+
+  while (i < len && !fn(xs[i])) i += 1
+  return _slice.call(xs, i)
+})
---
+++ tests/dropUntil.spec.js
@@ -0,0 +1,40 @@
+const test       = require('ava')
+    , { dropUntil } = require('../dist/fp-standard')
+
+test('properly reports its arity (is binary)', (t) => {
+  t.is(dropUntil.length, 2)
+})
+
+test('is curried', (t) => {
+  t.is(typeof dropUntil(() => {}), 'function')
+})
+
+test('drops all elements of an array until the predicate is met', (t) => {
+  const isEven = x => x % 2 === 0
+
+  t.deepEqual(dropUntil(isEven, [1, 3, 4, 6, 8]), [4, 6, 8])
+})
+
+test('returns an empty array if no items match the predicate', (t) => {
+  t.deepEqual(dropUntil(() => false, [1, 2, 3]), [])
+})
+
+test('returns all elements if the predicate returns true for the first element', (t) => {
+  t.deepEqual(dropUntil(() => true, [1, 2, 3]), [1, 2, 3])
+})
+
+test('does not mutate the original array', (t) => {
+  const isEven = x => x % 2 === 0
+      , xs = [1, 3, 4, 6, 8]
+
+  t.deepEqual(dropUntil(isEven, xs), [4, 6, 8])
+  t.deepEqual(xs, [1, 3, 4, 6, 8])
+})
+
+test('returns a copy of the array even if it is identical', (t) => {
+  const as = [1, 2, 3, 4]
+      , bs = dropUntil(() => true, as)
+
+  t.deepEqual(as, bs)
+  t.not(as, bs)
+})
