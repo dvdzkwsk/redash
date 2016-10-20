cab6e7ca5737103d64c245b2a7f364a25e61e052
David Zukowski <zukoweb@gmail.com>
2016-10-20 16:02:08 -0500

    feat(isOdd): add function

---
+++ src/isOdd.js
@@ -0,0 +1,17 @@
+/**
+ * @name isOdd
+ * @signature Number -> Boolean
+ * @since v0.14.0
+ * @description
+ * Returns whether or not a number is odd.
+ * @see isEven
+ *
+ * @example
+ * isOdd(3)        // => true
+ * isOdd(0)        // => false
+ * isOdd(3.9)      // => false
+ * isOdd(Infinity) // => false
+ */
+export default function isOdd (a) {
+  return !isNaN(a) && isFinite(a) && !!a && Math.floor(a) % 2 !== 0
+}
---
+++ tests/isOdd.spec.js
@@ -0,0 +1,39 @@
+const test     = require('ava')
+  ,  { isOdd } = require('../dist/fp-standard')
+
+test('properly reports its arity (is unary)', (t) => {
+  t.is(isOdd.length, 1)
+})
+
+test('returns true for odd numbers', (t) => {
+  t.true(isOdd(1))
+  t.true(isOdd(3))
+  t.true(isOdd(5))
+})
+
+test('returns false for even numbers', (t) => {
+  t.false(isOdd(2))
+  t.false(isOdd(4))
+  t.false(isOdd(6))
+})
+
+test('returns false for NaN', (t) => {
+  t.false(isOdd(NaN))
+})
+
+test('returns false if the number is not finite', (t) => {
+  t.false(isOdd(Infinity))
+  t.false(isOdd(-Infinity))
+})
+
+test('returns false for non-integers', (t) => {
+  t.false(isOdd(2.1))
+  t.false(isOdd(4.5))
+  t.false(isOdd(6.1))
+})
+
+test('returns false for 0', (t) => {
+  t.false(isOdd(0))
+  t.false(isOdd(+0))
+  t.false(isOdd(-0))
+})
