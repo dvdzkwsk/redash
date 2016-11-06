2cf7bcb2291c13547fa426e39f9e91ff57af8993
David Zukowski <zukoweb@gmail.com>
2016-11-06 12:30:37 -0600

    feat(when): add function

---
+++ src/when.js
@@ -0,0 +1,24 @@
+import _curry2 from './internal/_curry2'
+
+/**
+ * @name when
+ * @signature (a -> Boolean) -> (a -> *) -> a -> *
+ * @since v0.14.0
+ * @description
+ * Creates a unary function that compares its argument against the predicate
+ * function, and, if it is met, calls the transformation function and returns
+ * its result. If the predicate is not matched, the original argument is
+ * returned.
+ *
+ * @example
+ * const doubleIfEven = when(isEven, multiply(2))
+
+ * doubleIfEven(4) // => 8
+ * doubleIfEven(5) // => 5
+ * map(doubleIfEven, [1, 2, 3, 4, 5]) // => [1, 4, 3, 8, 5]
+ */
+export default _curry2(function when (pred, fn) {
+  return function (x) {
+    return pred(x) ? fn(x) : x
+  }
+})
---
+++ tests/when.spec.js
@@ -0,0 +1,22 @@
+const test     = require('ava')
+    , { when } = require('../dist/fp-standard')
+
+test('properly reports its arity (is binary)', (t) => {
+  t.is(when.length, 2)
+})
+
+test('is curried', (t) => {
+  t.is(typeof when(() => {}), 'function')
+})
+
+test('creates a unary function', (t) => {
+  t.is(when(() => {}, () => {}).length, 1)
+})
+
+test('returns the result of the transformation if the predicate is met', (t) => {
+  t.is(when(x => x > 3, x => x * 2)(4), 8)
+})
+
+test('returns the original argument if the predicate is not met', (t) => {
+  t.is(when(x => x > 3, x => x * 2)(2), 2)
+})
