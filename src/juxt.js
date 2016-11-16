58712e85378b278151b4040f9fb31ea2d506a851
David Zukowski <zukoweb@gmail.com>
2016-11-16 11:18:52 -0600

    feat(juxt): add function

---
+++ src/juxt.js
@@ -0,0 +1,22 @@
+import _slice from './internal/_slice'
+import map from './map'
+
+/**
+ * @name juxt
+ * @signature [(x, y, z... -> a), (x, y, z... -> b), ...] -> (x, y, z...) -> [a, b, ...]
+ * @description
+ * Applies a set of arguments to a list of functions, returning an array that
+ * contains the result of each function call in its corresponding position.
+ *
+ * @example
+ * juxt([inc, dec, multiply(3)])(2) => [3, 1, 6]
+ * juxt([add, divide, multiply])(2, 4) => [6, 2, 8]
+ */
+export default function juxt (fns) {
+  return function () {
+    const args = _slice.call(arguments)
+    return map(function (fn) {
+      return fn.apply(null, args)
+    }, fns)
+  }
+}
---
+++ tests/juxt.spec.js
@@ -0,0 +1,28 @@
+const test       = require('ava')
+    , { juxt
+      , add
+      , multiply
+      , divide
+      , inc
+      , dec
+    }            = require('../dist/redash')
+
+test('properly reports its arity (is unary)', (t) => {
+  t.is(juxt.length, 1)
+})
+
+test('correctly applies a single value to a list of functions', (t) => {
+  t.deepEqual(juxt([
+    inc
+  , dec
+  , multiply(3)
+  ])(2), [3, 1, 6])
+})
+
+test('correct applies multiple values to a list of functions', (t) => {
+  t.deepEqual(juxt([
+    add
+  , divide
+  , multiply
+  ])(2, 4), [6, 2, 8])
+})
