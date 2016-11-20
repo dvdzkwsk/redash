86522d87f21fb8900a7eb9664c127a71fd0cc4cd
David Zukowski <zukoweb@gmail.com>
2016-11-20 11:37:35 -0600

    feat(unless): add function

---
+++ src/unless.js
@@ -0,0 +1,21 @@
+import _curry2 from './internal/_curry2'
+import complement from './complement'
+import when from './when'
+
+/**
+ * @name unless
+ * @signature (a -> Boolean) -> (a -> b) -> a -> a | b
+ * @since v0.14.0
+ * @description
+ * Creates a unary function that dispatches to the provided function
+ * if the predicate returns false. Otherwise returns the received
+ * argument unmodified. This is the complement of `when`.
+ *
+ * @example
+ * const doubleIfNotEven = unless(isEven, multiply(2))
+ * doubleIfNotEven(3) // => 6
+ * doubleIfNotEven(2) // => 2
+ */
+export default _curry2(function unless (pred, fn) {
+  return when(complement(pred), fn)
+})
---
+++ tests/unless.spec.js
@@ -0,0 +1,23 @@
+const test       = require('ava')
+    , { unless } = require('../dist/redash')
+
+test('properly reports its arity (is binary)', (t) => {
+  t.is(unless.length, 2)
+})
+
+test('is curried', (t) => {
+  t.is(typeof unless(() => {}), 'function')
+})
+
+test('returns a unary function', (t) => {
+  t.is(typeof unless(() => {}, () => {}), 'function')
+  t.is(unless(() => {}, () => {}).length, 1)
+})
+
+test('dispatches to the provided fn if the predicate returns false', (t) => {
+  t.is(unless(x => x < 5, x => x * 2)(6), 12)
+})
+
+test('returns the argument if the predicate returns true', (t) => {
+  t.is(unless(x => x < 5, x => x * 2)(2), 2)
+})
