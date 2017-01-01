850adbbc8f8e841002f4e9cbefd254a6f1c8d791
David Zukowski <zukoweb@gmail.com>
2017-01-01 13:56:12 -0600

    fix(curry): do not restrict arguments to curried userland functions

--- src/curryN.js
+++ src/curryN.js
@@ -1,6 +1,4 @@
-import _curry1 from './internal/_curry1'
 import _curry2 from './internal/_curry2'
-import _curry3 from './internal/_curry3'
 import _curryN from './internal/_curryN'
 
 /**
@@ -19,11 +17,5 @@ import _curryN from './internal/_curryN'
  * fn(1)(2)(3) // => [1, 2, 3]
  */
 export default _curry2(function curryN (arity, fn) {
-  switch (arity) {
-    case 0: return fn
-    case 1: return _curry1(fn)
-    case 2: return _curry2(fn)
-    case 3: return _curry3(fn)
-    default: return _curryN(arity, [], fn)
-  }
+  return _curryN(arity, [], fn)
 })
