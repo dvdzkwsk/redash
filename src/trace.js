a0fb8c3ff954afaeacf8487e9de066563057de56
David Zukowski <zukoweb@gmail.com>
2017-01-06 08:35:58 -0600

    feat(trace): add function

---
+++ src/trace.js
@@ -0,0 +1,30 @@
+import tap from './tap'
+
+/**
+ * @name trace
+ * @signature String -> a -> a
+ * @since v0.18.0
+ * @description
+ * Creates a unary function that will log out the provided message along with its
+ * argument before returning that argument unmodified. This performs a side-effect
+ * (logging) and is useful for debugging composed functions by allowing you to
+ * trace how values change over time. Trace is solely meant as developer utility.
+ * @example
+ * map(pipe([
+ *   add(1),
+ *   trace('value after add'),
+ *   multiply(2),
+ *   trace('value after multiply')
+ * ]), [1, 2, 3, 4])
+ * // => [4, 6, 8, 10]
+ * // Logs:
+ * // value after add 2
+ * // value after add 3
+ * // ...
+ * // value after multiply 10
+ */
+export default function trace (message) {
+  return tap(function (x) {
+    console.log(message, x)
+  })
+}
