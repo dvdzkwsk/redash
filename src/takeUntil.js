5ec91fe1656cf2d95d132221f2debb71bc7da43d
David Zukowski <zukoweb@gmail.com>
2017-01-21 11:51:25 -0600

    feat(takeUntil): add function

---
+++ src/takeUntil.js
@@ -0,0 +1,23 @@
+import _curry2 from './internal/_curry2'
+import complement from './complement'
+import takeWhile from './takeWhile'
+
+/**
+ * @name takeUntil
+ * @signature (a -> Boolean) -> [a] -> [a]
+ * @since v0.12.0
+ * @description
+ * Selects values from the input list by comparing them against the predicate.
+ * Values are included in the output list so long as they do not fulfill the
+ * predicate. Once the predicate returns true, that value and the rest of
+ * the list is discarded.
+ *
+ * If the predicate never returns true, a copy of the entire list is returned.
+ * @see takeWhile
+ *
+ * @example
+ * takeUntil(isEven, [1, 3, 5, 2, 7]) // => [1, 3, 5]
+ */
+export default _curry2(function takeUntil (fn, xs) {
+  return takeWhile(complement(fn), xs)
+})
---
+++ tests/takeUntil.spec.js
@@ -0,0 +1,36 @@
+const test          = require('ava')
+    , sinon         = require('sinon')
+    , { takeUntil } = require('../dist/redash')
+
+test('properly reports its arity (is binary)', (t) => {
+  t.is(takeUntil.length, 2)
+})
+
+test('is curried', (t) => {
+  t.is(typeof takeUntil(() => {}), 'function')
+})
+
+test('returns a new array containing all the items up until the predicate returned true', (t) => {
+  t.deepEqual(takeUntil(x => x === 5, [1, 2, 3, 4, 5, 6, 7]), [1, 2, 3, 4])
+})
+
+test('short circuits', (t) => {
+  const spy = sinon.spy(x => x === 5)
+
+  takeUntil(spy, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+  t.is(spy.callCount, 5)
+})
+
+test('return a new array even if it\'s identical to the input', (t) => {
+  const arr = [1, 2, 3, 4]
+      , res = takeUntil(() => false, [1, 2, 3, 4])
+
+  t.deepEqual(arr, [1, 2, 3, 4])
+  t.deepEqual(res, [1, 2, 3, 4])
+  t.not(arr, res)
+})
+
+// asserts that the slice does not slice back onto a negative index
+test('return an empty list if the first item passes the predicate', (t) => {
+  t.deepEqual(takeUntil(() => true, [1, 2, 3, 4]), [])
+})
