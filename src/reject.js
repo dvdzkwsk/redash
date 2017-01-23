0a7ceffca7ae1561725fafe67fe167a792c5b3de
David Zukowski <zukoweb@gmail.com>
2017-01-23 09:17:55 -0600

    refactor(reject): implement as filter

--- src/reject.js
+++ src/reject.js
@@ -1,8 +1,12 @@
 import _curry2 from './internal/_curry2'
+import complement from './complement'
+import filter from './filter'
 
 /**
  * @name reject
- * @signature (a -> Boolean) -> [a] -> [a]
+ * @signature
+ * (a -> Boolean) -> [a] -> [a]
+ * String k, Any v => (v -> Boolean) -> {k:v} -> {k:v}
  * @since v0.1.0
  * @description
  * Returns a subset of a list which excludes all items for which the
@@ -12,17 +16,6 @@ import _curry2 from './internal/_curry2'
  * @example
  * reject(isEven, [1, 2, 3, 4, 5]) // => [1, 3, 5]
  */
-export default _curry2(function reject (pred, as) {
-  var i   = 0
-    , len = as.length
-    , res = []
-    , a
-
-  while (i < len) {
-    a = as[i++]
-    if (!pred(a)) {
-      res[res.length] = a
-    }
-  }
-  return res
+export default _curry2(function reject (pred, xs) {
+  return filter(complement(pred), xs)
 })
--- tests/reject.spec.js
+++ tests/reject.spec.js
@@ -12,3 +12,10 @@ test('is curried', (t) => {
 test('returns an array containing only the items where the predicate returned false', (t) => {
   t.deepEqual(reject(x => x < 3, [1, 2, 3, 4, 5]), [3, 4, 5])
 })
+
+test('works for objects', (t) => {
+  t.deepEqual(
+    reject(x => x > 5, { a: 1, b: 6, c: 5, d: 7 }),
+    { a: 1, c: 5 }
+  )
+})
