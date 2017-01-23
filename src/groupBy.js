a67fbef15fcf4125c75112cc9018e0b3affde1dc
David Zukowski <zukoweb@gmail.com>
2017-01-23 09:46:11 -0600

    feat(groupBy): add function

---
+++ src/groupBy.js
@@ -0,0 +1,33 @@
+import _curry2 from './internal/_curry2'
+import _forEach from './internal/_forEach'
+
+/**
+ * @name groupBy
+ * @signature
+ * String k, Any v => (v -> k) -> [v] -> {k:v}
+ * @since v0.18.0
+ * @description
+ * Creates an object from a list of values where the values are grouped
+ * together as a list based on the key returned from the grouping function.
+ *
+ * @example
+ * groupBy(
+ *   x => x.age <= 12 ? 'child' : x.age < 18 ? 'teenager' : 'adult',
+ *   [{ age: 5 }, { age: 7 }, { age: 21 }, { age: 15 }, { age: 2 }]
+ * )
+ * // =>
+ * // {
+ * //   child: [{ age: 5 }, { age: 7 }, { age: 2 }]
+ * // , teenager: [{ age: 15 }]
+ * // , adult: [{ age: 21 }]
+ * // }
+ */
+export default _curry2(function groupBy (fn, xs) {
+  var res = {}
+
+  _forEach(function (x) {
+    var key = fn(x)
+    res[key] = (res[key] || []).concat(x)
+  }, xs)
+  return res
+})
---
+++ tests/groupBy.spec.js
@@ -0,0 +1,28 @@
+const test       = require('ava')
+    , { groupBy } = require('../dist/redash')
+
+test('properly reports its arity (is binary)', (t) => {
+  t.is(groupBy.length, 2)
+})
+
+test('is curried', (t) => {
+  t.is(typeof groupBy(() => {}), 'function')
+})
+
+test('correctly groups items by key', (t) => {
+  t.deepEqual(
+    groupBy(
+      x => x.age <= 12 ? 'child' : x.age < 18 ? 'teenager' : 'adult',
+      [{ age: 5 }, { age: 7 }, { age: 21 }, { age: 15 }, { age: 2 }]
+    ),
+    {
+      child: [{ age: 5 }, { age: 7 }, { age: 2 }]
+    , teenager: [{ age: 15 }]
+    , adult: [{ age: 21 }]
+    }
+  )
+})
+
+test('returns an empty object if the list is empty', (t) => {
+  t.deepEqual(groupBy(() => 'a', []), {})
+})
