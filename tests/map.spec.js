053b6ce044215742fd0dd43993ca7c78dbb58a0c
David Zukowski <zukoweb@gmail.com>
2017-02-20 08:46:34 -0600

    deprecate(mapValues): use `map` instead

--- tests/map.spec.js
+++ tests/map.spec.js
@@ -10,11 +10,13 @@ test('is curried', (t) => {
   t.is(typeof map(() => {}), 'function')
 })
 
-test('returns a new array where each element has been transformed in place', (t) => {
+// Lists
+// ------------------------------------
+test('List => returns a new list where each element has been transformed in place', (t) => {
   t.deepEqual(map(x => x * 2, [1, 2, 3, 4, 5]), [2, 4, 6, 8, 10])
 })
 
-test('provides a single argument (the element) to the transformer', (t) => {
+test('List => provides a single argument (the element) to the transformer', (t) => {
   const spy = sinon.spy()
 
   map(spy, [1, 2, 3])
@@ -23,7 +25,7 @@ test('provides a single argument (the element) to the transformer', (t) => {
   t.deepEqual(spy.thirdCall.args, [3])
 })
 
-test('returns a new array even if the result is identical', (t) => {
+test('List => returns a new list even if the result is identical', (t) => {
   const arr = [1, 2, 3]
       , res = map(x => x, arr)
 
@@ -32,6 +34,38 @@ test('returns a new array even if the result is identical', (t) => {
   t.not(arr, res)
 })
 
-test('maps over objects', (t) => {
+// Objects
+// ------------------------------------
+test('Object => maps over objects', (t) => {
   t.deepEqual(map(x => x * 2, { a: 1, b: 2, c: 3 }), { a: 2, b: 4, c: 6 })
 })
+
+test('Object => calls the transformer with a single argument: value', (t) => {
+  const spy = sinon.spy()
+  map(spy, { a: 1 })
+  t.true(spy.calledWithExactly(1))
+})
+
+test('Object => transforms own enumerable properties', (t) => {
+  t.deepEqual(
+    map(x => x * 2, { a: 1, b: 2 }),
+    { a: 2, b: 4 })
+})
+
+test('Object => ignores inherited properties', (t) => {
+  function A () {}
+  A.prototype.foo = () => {}
+  const a = new A()
+  a.bar = 5
+
+  t.deepEqual(map(x => x * 2, a), { bar: 10 })
+})
+
+test('Object => always returns a new object', (t) => {
+  const obj = { a: 1, b: 2, c: 3 }
+      , res = map(x => x, obj)
+
+  t.deepEqual(res, obj) // compare values
+  t.not(res, obj)       // compare references
+})
+
