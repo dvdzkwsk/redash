09c68bbe7a4a5710bdfd53cd9e23dcca6b45adda
David Zukowski <zukoweb@gmail.com>
2017-02-20 09:30:27 -0600

    refactor(insert): remove internal _push helper

--- src/insert.js
+++ src/insert.js
@@ -1,5 +1,4 @@
 import _curry3 from './internal/_curry3'
-import _push from './internal/_push'
 import _slice from './internal/_slice'
 
 /**
@@ -19,6 +18,5 @@ import _slice from './internal/_slice'
 export default _curry3(function insert (idx, x, xs) {
   var ys = _slice.call(xs, 0, idx)
   ys[ys.length] = x
-  _push.apply(ys, _slice.call(xs, idx))
-  return ys
+  return ys.concat(_slice.call(xs, idx))
 })
--- tests/insert.spec.js
+++ tests/insert.spec.js
@@ -14,6 +14,10 @@ test('inserts the element at the target index with the new value', (t) => {
   t.deepEqual(insert(4, 100, [1, 2, 3, 4, 5]), [1, 2, 3, 4, 100, 5])
 })
 
+test('does not flatten inserted arrays', (t) => {
+  t.deepEqual(insert(3, [4, 5, 6], [1, 2, 3, 7, 8]), [1, 2, 3, [4, 5, 6], 7, 8])
+})
+
 test('does not mutate the original array', (t) => {
   const arr = [1, 2, 3, 4, 5]
       , res = insert(2, 100, arr)
