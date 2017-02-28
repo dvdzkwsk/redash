48165817abfaca8925b02ef070ffd7307a8e4500
David Zukowski <zukoweb@gmail.com>
2017-02-28 11:41:35 -0600

    feat(padLeft): add function

---
+++ src/padLeft.js
@@ -0,0 +1,21 @@
+import _defn from './internal/_defn'
+
+/**
+ * @name padLeft
+ * @signature Integer -> String -> String -> String
+ * @since v0.19.0
+ * @description
+ * Pads a string to a given length by prepending it with the given character
+ * until that length is reached.
+ *
+ * @example
+ * padLeft(5, 'X', 'hi') // => 'XXXhi'
+ */
+// TODO(zuko): how should we handle char of length > 1?
+// TODO(zuko): throw error if char is empty?
+export default _defn('padLeft', function (length, char, str) {
+  while (str.length < length) {
+    str = char + str
+  }
+  return str
+})
---
+++ tests/padLeft.spec.js
@@ -0,0 +1,22 @@
+const test        = require('ava')
+    , { padLeft } = require('../dist/halcyon')
+
+test('properly reports its arity (is ternary)', (t) => {
+  t.is(padLeft.length, 3)
+})
+
+test('is curried', (t) => {
+  t.is(typeof padLeft(5, ' '), 'function')
+})
+
+test('pads the string to the target length', (t) => {
+  t.is(padLeft(5, 'X', 'hi'), 'XXXhi')
+})
+
+test('noops if the string is already at the target length', (t) => {
+  t.is(padLeft(5, 'X', 'hello'), 'hello')
+})
+
+test('noops if the string is already above the target length', (t) => {
+  t.is(padLeft(3, 'X', 'hello'), 'hello')
+})
