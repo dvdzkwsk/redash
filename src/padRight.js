155870dabcf8c9f360a20980391d0bbf04d5df87
David Zukowski <zukoweb@gmail.com>
2017-02-28 11:44:07 -0600

    feat(padRight): add function

---
+++ src/padRight.js
@@ -0,0 +1,21 @@
+import _defn from './internal/_defn'
+
+/**
+ * @name padRight
+ * @signature Integer -> String -> String -> String
+ * @since v0.19.0
+ * @description
+ * Pads a string to a given length by appending the given character to it
+ * until that length is reached.
+ *
+ * @example
+ * padRight(5, 'X', 'hi') // => 'hiXXX'
+ */
+// TODO(zuko): how should we handle char of length > 1?
+// TODO(zuko): throw error if char is empty?
+export default _defn('padRight', function (length, char, str) {
+  while (str.length < length) {
+    str += char
+  }
+  return str
+})
---
+++ tests/padRight.spec.js
@@ -0,0 +1,22 @@
+const test         = require('ava')
+    , { padRight } = require('../dist/halcyon')
+
+test('properly reports its arity (is ternary)', (t) => {
+  t.is(padRight.length, 3)
+})
+
+test('is curried', (t) => {
+  t.is(typeof padRight(5, ' '), 'function')
+})
+
+test('pads the string to the target length', (t) => {
+  t.is(padRight(5, 'X', 'hi'), 'hiXXX')
+})
+
+test('noops if the string is already at the target length', (t) => {
+  t.is(padRight(5, 'X', 'hello'), 'hello')
+})
+
+test('noops if the string is already above the target length', (t) => {
+  t.is(padRight(3, 'X', 'hello'), 'hello')
+})
