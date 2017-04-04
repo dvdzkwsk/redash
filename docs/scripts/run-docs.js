d27175db04abfad3eea3d918ae75d42dccdb3b8f
Levi Thomason <me@levithomason.com>
2017-04-04 13:42:48 -0700

    fix(docs): add app_template configuration

--- docs/scripts/run-docs.js
+++ docs/scripts/run-docs.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 require('genesis-core').dev({
   root: path.resolve(__dirname, '..'),
+  app_template: path.resolve(__dirname, '../src/index.html'),
   main: [
     path.resolve(__dirname, '../src/main.preload.js'),
     path.resolve(__dirname, '../src/main.js'),
