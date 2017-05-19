87e70308b894042d9d9cf5ca12dfe7b8a98379fa
David Zukowski <zukoweb@gmail.com>
2017-05-18 19:09:16 -0500

    refactor(isEmpty): treat falsy values as empty

--- src/isEmpty.js
+++ src/isEmpty.js
@@ -11,20 +11,15 @@ import empty from './empty'
  * value is compared against its type's empty representation. Dispatches to
  * the `empty` method on an object if it exists.
  *
- * Note that this is different from checking whether or not a value is falsy.
- * Nil values (null and undefined) are not considered empty, nor is 0, since
- * these are discrete values that cannot contain anything.
- *
  * @example
  * isEmpty('')        // => true
  * isEmpty([])        // => true
  * isEmpty({})        // => true
- * isEmpty(null)      // => false
- * isEmpty(undefined) // => false
- * isEmpty(0)         // => false
- * isEmpty(false)     // => false
+ * isEmpty(null)      // => true
+ * isEmpty(undefined) // => true
+ * isEmpty(0)         // => true
+ * isEmpty(false)     // => true
  */
 export default _defn('isEmpty', function (a) {
-  if (a == null) return false
-  return _equals(a, empty(a))
+  return !a || _equals(a, empty(a))
 })
--- tests/isEmpty.spec.js
+++ tests/isEmpty.spec.js
@@ -13,16 +13,16 @@ test('returns false for an array with > 0 items', (t) => {
   t.false(isEmpty([1, 2, 3]))
 })
 
-test('returns false for null',  (t) => {
-  t.false(isEmpty(null))
+test('returns true for null',  (t) => {
+  t.true(isEmpty(null))
 })
 
-test('returns false for undefined',  (t) => {
-  t.false(isEmpty(undefined))
+test('returns true for undefined',  (t) => {
+  t.true(isEmpty(undefined))
 })
 
-test('returns false for void 0',  (t) => {
-  t.false(isEmpty(void 0))
+test('returns true for void 0',  (t) => {
+  t.true(isEmpty(void 0))
 })
 
 test('returns true for a string with a length of 0', (t) => {
@@ -33,8 +33,16 @@ test('returns false for a string with a length > 0', (t) => {
   t.false(isEmpty('a'))
 })
 
-test('returns false for 0', (t) => {
-  t.false(isEmpty(0))
+test('returns true for 0', (t) => {
+  t.true(isEmpty(0))
+})
+
+test('returns true for false', (t) => {
+  t.true(isEmpty(false))
+})
+
+test('returns false for true', (t) => {
+  t.false(isEmpty(true))
 })
 
 test('returns true for an object with 0 keys', (t) => {
