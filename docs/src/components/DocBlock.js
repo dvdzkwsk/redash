46aebe65ec78b98af5c9ef62e56ea3051adc457b
David Zukowski <zukoweb@gmail.com>
2017-08-20 09:48:26 -0500

    feat(docs): do not tokenize examples for SSR

--- docs/src/components/DocBlock.js
+++ docs/src/components/DocBlock.js
@@ -15,14 +15,87 @@ const pure = (WrappedComponent) => {
   }
 }
 
+const debounce = (ms, fn) => {
+  let timeout
+  return (...args) => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => fn(...args), ms)
+  }
+}
 
-const Example = ({ children }) => (
-  <pre className='doc-card__example language-javascript'>
-    <code
-      dangerouslySetInnerHTML={{ __html: children }}
-    />
-  </pre>
-)
+let onScroll
+try {
+  let listeners = []
+  window.addEventListener('scroll', debounce(150, () => {
+    if (!listeners.length) return
+    const winHeight = window.innerHeight
+    listeners.slice().forEach(fn => fn(winHeight))
+  }))
+  onScroll = (fn) => {
+    listeners.push(fn)
+    return () => {
+      listeners = listeners.filter(f => f !== fn)
+    }
+  }
+} catch (e) {
+  onScroll = () => () => {}
+}
+
+class Example extends Component {
+  constructor (props, ctx) {
+    super(props, ctx)
+    this.state = {
+      highlighted: false,
+      content: props.children[0],
+    }
+  }
+
+  componentDidMount () {
+    this._highlight()
+  }
+
+  componentWillUnmount () {
+    this._destroyScrollWatch && this._destroyScrollWatch()
+  }
+
+  _onViewScroll = (windowHeight) => {
+    this._highlightIfNeeded(windowHeight)
+  }
+
+  _onRef = (node) => {
+    return // temporarily highlighting everything in prod
+    this._node = node
+    this._destroyScrollWatch = onScroll(this._onViewScroll)
+    this._highlightIfNeeded(window.innerHeight)
+  }
+
+  _highlightIfNeeded = (windowHeight) => {
+    if (!this._node || this.state.highlighted) return
+
+    const nodeRect = this._node.getBoundingClientRect()
+    if (nodeRect.top > 0 && nodeRect.top < windowHeight) {
+      this._highlight()
+    }
+  }
+
+  _highlight = () => {
+    this._destroyScrollWatch && this._destroyScrollWatch()
+    this.setState({
+      highlighted: true,
+      content: Prism.highlight(this.state.content, Prism.languages.javascript),
+    })
+  }
+
+  render () {
+    let className = 'doc-card__example '
+    className += this.state.highlighted ? 'language-javascript' : 'language-none'
+    return (
+      <pre className={className} ref={this._onRef}>
+        <code dangerouslySetInnerHTML={{ __html: this.state.content }} />
+      </pre>
+    )
+  }
+}
 
 const DocBlock = ({ name, signature, description, examples, since, see }) => {
   const id = toLower(name)
@@ -56,9 +129,7 @@ const DocBlock = ({ name, signature, description, examples, since, see }) => {
       )}
       <hr />
       {mapi((example, i) => (
-        <Example key={i}>
-          {Prism.highlight(example, Prism.languages.javascript)}
-        </Example>
+        <Example key={i}>{example}</Example>
       ), examples)}
     </div>
   )
